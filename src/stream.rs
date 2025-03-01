47b8abcef9d9d478508c894a3de7012cba3b4665
kinjalkt3 <kinjalk.tripathi@jkcsoftwares.com>
2025-03-01 10:29:50 +0530

    rm

--- src/stream.rs
+++ src/stream.rs
@@ -1,5 +1,4 @@
 use futures::{SinkExt, StreamExt};
-use opencv::imgproc;
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
@@ -400,38 +399,19 @@ fn send_frame(
     broadcaster: &WebSocketBroadcaster,
     round_id: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Sending frame - original size: {:?}", frame.size()?);
-
-    // Let's try sending the frame directly first without color conversion
     let mut buffer = Vector::new();
-    let params = Vector::from_slice(&[imgcodecs::IMWRITE_JPEG_QUALITY, 90]);
+    imgcodecs::imencode(".jpg", &frame, &mut buffer, &Vector::new())?;
+    let frame_data = base64::encode(&buffer);
 
-    match imgcodecs::imencode(".jpg", &frame, &mut buffer, &params) {
-        Ok(_) => {
-            println!("Successfully encoded frame, buffer size: {}", buffer.len());
-            let frame_data = base64::encode(&buffer);
+    let frame_response = ProcessResponse::Frame {
+        frame_number: processor.get_frame_number()?,
+        frame_data,
+        total_frames: processor.get_total_frames()?,
+    };
 
-            let frame_response = ProcessResponse::Frame {
-                frame_number: processor.get_frame_number()?,
-                frame_data,
-                total_frames: processor.get_total_frames()?,
-            };
-
-            match broadcaster.broadcast(round_id, &frame_response) {
-                Ok(_) => println!("Successfully broadcasted frame"),
-                Err(e) => println!("Error broadcasting frame: {:?}", e),
-            }
-
-            Ok(())
-        }
-        Err(e) => {
-            println!("Error encoding frame: {:?}", e);
-            Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                format!("Frame encoding failed: {:?}", e),
-            )))
-        }
-    }
+    // println!("Broadcasting frame to round: {}", round_id);
+    broadcaster.broadcast(round_id, &frame_response)?;
+    Ok(())
 }
 
 fn send_response(
